5cc545137df15365086b4d2715438f9585a284ad
bryan.parrish <bryan.parrish@asu.edu>
2017-02-17 20:14:53 -0700

    Code  Standards - 3 done
    All methods now have the template comment.  Did not do tests since it
    does not make sense...

--- src/banking/primitive/core/Checking.java
+++ src/banking/primitive/core/Checking.java
@@ -5,22 +5,46 @@ public class Checking extends Account {
 	private static final long _serialVersionUID = 11L;
 	private int _numWithdraws = 0;
 	
+	/**
+	Method: Checking Constructor
+	Inputs: String name
+	Returns: none
+
+	Description: Creates a new Checking object with the name of the account 
+	*/
 	private Checking(String name) {
 		super(name);
 	}
 
+	/**
+	Method: createChecking
+	Inputs: String name
+	Returns: A new Checking object
+
+	Description: Creates a new checking object with the parameter name
+	*/
     public static Checking createChecking(String name) {
         return new Checking(name);
     }
 
+    /**
+    Method: Checking Constructor
+    Inputs: String name, float balance
+    Returns: none
+
+    Description: Creates a new Checking object with the name and balance for the account
+    */
 	public Checking(String name, float balance) {
 		super(name, balance);
 	}
 
 	/**
-	 * A deposit may be made unless the Checking account is closed
-	 * @param float is the deposit amount
-	 */
+	Method: deposit
+	Inputs: float amount to be deposited
+	Returns: boolean true if successful, false if not
+
+	Description: A deposit may be made unless the Checking account is closed
+	*/
 	public boolean deposit(float amount) {
 		if (getState() != State.CLOSED && amount > 0.0f) {
 			balance = balance + amount;
@@ -33,9 +57,12 @@ public class Checking extends Account {
 	}
 
 	/**
-	 * Withdrawal. After 10 withdrawals a fee of $2 is charged per transaction You may 
-	 * continue to withdraw an overdrawn account until the balance is below -$100
-	 */
+	Method: Withdrawal
+	Inputs: float amount to be withdrawn
+	Returns: boolean true if successful, false if not
+
+	Description: After 10 withdrawals a fee of $2 is charged per transaction You may continue to withdraw an overdrawn account until the balance is below -$100
+	*/
 	public boolean withdraw(float amount) {
 		if (amount > 0.0f) {		
 			// KG: incorrect, last balance check should be >=
@@ -53,10 +80,20 @@ public class Checking extends Account {
 		return false;
 	}
 
+	/**
+	 * Getter
+	 */
 	public String getType() { 
 		return "Checking"; 
 	}
 	
+	/**
+	Method: toString
+	Inputs: none
+	Returns: String proper formatted String object
+
+	Description: Will return a formatted Checking object as a String
+	*/
 	public String toString() {
 		return "Checking: " + getName() + ": " + getBalance();
 	}
--- src/banking/primitive/core/Savings.java
+++ src/banking/primitive/core/Savings.java
@@ -5,17 +5,35 @@ public class Savings extends Account {
 	private static final long _SERIALVERSIONUID = 111L;
 	private int _numWithdraws = 0;
 
+	/**
+	Method: Savings Constructor
+	Inputs: String name of the account
+	Returns: none
+
+	Description: Creates a new Savings object with a name
+	*/
 	public Savings(String name) {
 		super(name);
 	}
 
+	/**
+	Method: Savings Constructor
+	Inputs: String name of the account, float balance of the account
+	Returns: none
+
+	Description: Creates a new Savings object with a name and the balance
+	*/
 	public Savings(String name, float balance) throws IllegalArgumentException {
 		super(name, balance);
 	}
 
 	/**
-	 * A deposit comes with a fee of 50 cents per deposit
-	 */
+	Method: deposit
+	Inputs: float amount to be deposited
+	Returns: boolean true if the deposit was successful, otherwise false
+
+	Description: A deposit comes with a fee of 50 cents per deposit
+	*/
 	public boolean deposit(float amount) {
 		if (getState() != State.CLOSED && amount > 0.0f) {
 			balance = balance + amount - 0.50F;
@@ -27,9 +45,12 @@ public class Savings extends Account {
 	}
 
 	/**
-	 * A withdrawal. After 3 withdrawals a fee of $1 is added to each withdrawal.
-	 * An account whose balance dips below 0 is in an OVERDRAWN state
-	 */
+	Method: withdrawl
+	Inputs: float amount to be withdrawn
+	Returns: boolean true if withdraw was successful
+
+	Description: A withdrawal. After 3 withdrawals a fee of $1 is added to each withdrawal. An account whose balance dips below 0 is in an OVERDRAWN state
+	*/
 	public boolean withdraw(float amount) {
 		if (getState() == State.OPEN && amount > 0.0f) {
 			balance = balance - amount;
@@ -50,6 +71,13 @@ public class Savings extends Account {
 	
 	public String getType() { return "Checking"; }
 
+	/**
+	Method: toString
+	Inputs: none
+	Returns: Formatted string object
+
+	Description: Returns a Savings object formatted as a string
+	*/
 	public String toString() {
 		return "Savings: " + getName() + ": " + getBalance();
 	}
